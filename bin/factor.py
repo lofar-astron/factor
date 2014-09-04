e71967325ff209339ebdb639c2bd06f06572d730
revoltek <rev@voo.it>
2014-09-04 15:55:32 +0200

    import

--- bin/factor.py
+++ bin/factor.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+# Authors:
+# Francesco de Gasperin
+# Jose Sabater Montes
+# Wendy Williams
+# Martin Hardcastle
+#
+# the code is based on an original idea of Reinout van Weeren
+
+_author = "Francesco de Gasperin (fdg@hs.uni-hamburg.de)"
+
+import sys, os, time
+import logging
+import ..factor._version
+import ..factor._logging
