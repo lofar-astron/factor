86d6d6a9124a1f157a9441cb7c8a610f65f7e5e6
revoltek <rev@voo.it>
2014-09-05 15:46:12 +0200

    move file

---
+++ factor/lib/operations.py
@@ -0,0 +1,28 @@
+"""
+General operation library, contains the master class for operations
+"""
+
+class operation():
+    """
+    General class for operations. All operations should be in a separate module.
+    Every module must have a class called in the same way of the module which
+    inherits from this class.
+    """
+    import logging
+
+    def __init__(self, parset):
+        self.parset = parset
+
+    def run(self):
+        pass
+
+class operation_facet(operation):
+    """
+    Extend the operation class for facet-specific operations
+    """
+
+    def __init__(self, parset, direction):
+        self.operation(parset)
+        self.direction = direction
+
+
