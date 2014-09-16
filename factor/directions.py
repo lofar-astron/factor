70cbd8cd8d91e83bd234a46228e83d2463053c1f
revoltek <rev@voo.it>
2014-09-16 16:21:34 +0200

    direction system

--- factor/directions.py
+++ factor/directions.py
@@ -1,6 +1,7 @@
 # This module handle the reading of directions
 
 import os
+import numpy as np
 import logging 
 from factor.lib.direction import direction as d
 
@@ -13,5 +14,13 @@ def directions_read(directions_file):
         sys.exit(1)
 
     log.info("Reading directions file: %s" % (directions_file))
+    types = np.dtype({'names':['name','ra','dec','reg','multiscale','solint_a','solint_p','hdr'], \
+                    'formats':['S100',np.float,np.float,'S100',np.bool,np.int,np.int,np.bool]})
+    directions = np.genfromtxt(directions_file, comments='#', delimiter=',', unpack=False,
+                      converters={0: lambda x: x.strip(), 3: lambda x: x.strip()}, dtype=types)
 
-    return [d('aaa'), d('bbb')]
+    data = []
+    for direction in directions:
+        data.append( d(*direction) )
+    
+    return data
--- factor/lib/direction.py
+++ factor/lib/direction.py
@@ -1,11 +1,20 @@
 """
 Definition of direction-related classes
 """
+import logging
 
 class direction( object ):
     """
     general direciton class
     """
-    def __init__(self, name):
+    def __init__(self, name, ra, dec, reg, multiscale, solint_a, solint_p, hdr):
+        logging.debug("Setting up direction %s." % name)
         self.name = name
+        self.ra = ra
+        self.dec = dec
+        self.reg = reg
+        self.multiscale = multiscale
+        self.solint_a = solint_a
+        self.solint_p = solint_p
+        self.hdr = hdr
 
