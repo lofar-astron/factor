0942926efadd23c9b247732df4aeba0cee29f379
revoltek <rev@voo.it>
2014-09-23 17:34:34 +0200

    param

---
+++ factor/operations/hardcoded_param.py
@@ -0,0 +1,19 @@
+# this file contains some hardcoded parameters
+# that can be easily edited by hand. The dict names
+# refer to a specific operation. Actions should have all
+# the parameters passed, not hardcoded
+
+init_subtract = {'niterh': 40000,
+                 'niterl' : 10000,
+                 'imsizeh': 6144,
+                 'imsizel': 4800,
+                 'cellh': '7.5arcsec',
+                 'celll': '25arcsec',
+                 'uvrangeh': "0.08~7.0klambda",
+                 'uvrangel': "0.08~2.0klambda",
+                 'threshislh': 2.5,
+                 'threshisll': 5,
+                 'threshpixh': 4,
+                 'threshpixl': 5,
+                 'nterms': 1}
+
