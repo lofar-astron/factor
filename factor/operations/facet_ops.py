8c7916ae111e44a03528113049df1e1849b4d3e8
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-03-02 17:21:57 +0100

    Fix typo

--- factor/operations/facet_ops.py
+++ factor/operations/facet_ops.py
@@ -255,7 +255,7 @@ class FacetSelfcal(Operation):
         chunks_list = []
         for d in d_list:
             chunks_list.append(self.make_chunks(d.concat_file, d.solint_a,
-            	self.parset 'facet_chunk', direction=d))
+            	self.parset, 'facet_chunk', direction=d))
         chunk_data_mapfiles = []
         chunk_parmdb_mapfiles = []
         chunk_model_mapfiles = []
