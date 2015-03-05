415173e94126860822620ac54c206a6a5f39dc04
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-03-05 08:55:42 +0100

    Fix paths

--- factor/actions/models.py
+++ factor/actions/models.py
@@ -444,7 +444,7 @@ class FFT(Action):
             os.makedirs(self.working_dir)
 
         # Define script and task files
-        self.fft_script_file = self.parsetbasename + 'ftw.py'
+        self.script_file = self.parsetbasename + 'ftw.py'
         self.task_xml_file = self.parsetbasename + 'ftw.xml'
         self.task_py_file = self.parsetbasename + 'task_ftw.py'
 
@@ -457,17 +457,6 @@ class FFT(Action):
 
         self.p['vis_datamap'] = self.vis_datamap
         self.p['model_datamap'] = self.model_datamap
-
-        # Make data map for facet region files
-        vis_files = read_mapfile(self.vis_datamap)
-        if self.direction is not None:
-            region_files = [self.direction.reg] * len(vis_files)
-            self.p['region_datamap'] = write_mapfile(region_files,
-            	self.op_name, self.name, prefix=self.prefix+'_region',
-            	direction=self.direction,
-            	working_dir=self.op_parset['dir_working'])
-        else:
-            self.p['region_datamap'] = None
 
 
     def make_pipeline_control_parset(self):
@@ -486,6 +475,7 @@ class FFT(Action):
         tmp = template.render(self.p)
         with open(self.task_xml_file, 'w') as f:
             f.write(tmp)
+
         self.p['task_py_file'] = self.task_py_file
         template = env.get_template('task_ftw.tpl')
         tmp = template.render(self.p)
--- factor/actions/templates/fft.pipeline.parset.tpl
+++ factor/actions/templates/fft.pipeline.parset.tpl
@@ -1,7 +1,8 @@
 pipeline.steps=[fft]
 
 fft.control.kind=recipe
-fft.control.type=casapy
-fft.control.opts.mapfiles_in=[{{ vis_datamap }}, {{model_datamap}}, {{ region_datamap }}]
-fft.control.opts.inputkeys=[inputms, modelimg, region]
+fft.control.type=executable_args
+fft.control.opts.executable=casa
+fft.control.opts.mapfiles_in=[{{ vis_datamap }}, {{model_datamap}}]
+fft.control.opts.inputkeys=[inputms, modelimg]
 fft.control.opts.arguments=[--nologger, --log2term, --nogui, -c, {{ scriptname }}, inputms, modelimg, {{ nterms }}, {{ imsize }}]
