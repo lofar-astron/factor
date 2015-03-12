86b7eaa2dfff898a9fe7bcb39fc8229e9929908b
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-03-12 14:10:23 +0100

    Fix typo

--- factor/directions.py
+++ factor/directions.py
@@ -261,7 +261,7 @@ def group_directions(directions, one_at_a_time=True, n_per_grouping={'1':0,
                 for i in range(0, len(direction_groups_orig), 2):
                     group1 = direction_groups_orig[i]
                     if i < len(direction_groups)-1:
-                        k = 1 + 1
+                        k = i + 1
                     else:
                         k = i - 1
                     group2 = direction_groups_orig[k]
