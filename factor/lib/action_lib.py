37d7f3839c40cc8f66130642d4f2c2af61029321
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-03-29 10:44:36 +0200

    Add restfreq to wsclean model img

--- factor/lib/action_lib.py
+++ factor/lib/action_lib.py
@@ -146,10 +146,12 @@ def convert_fits_to_image(fitsimage, force_stokes_I=True):
     Convert a fits image to a CASA image
 
     For WSClean 1.7, use force_stokes_I = True to overide incorrect Stokes
-    keyword in model image headers
+    keyword in model image headers. The restfreq is also set to avoid
+    problems with casapy2bbs.py
     """
     import pyrap.images as pim
     import pyrap.tables as pt
+    import numpy as np
 
     outfilename = fitsimage.split('.fits')[0] + '.image'
     casaimage = pim.image(fitsimage)
@@ -158,6 +160,8 @@ def convert_fits_to_image(fitsimage, force_stokes_I=True):
     if force_stokes_I:
         coords = casaimage.coordinates().dict()
         coords['stokes1']['stokes'] = ['I']
+        freq = coords['spectral2']['wcs']['crval']
+        coords['spectral2']['restfreqs'] = np.array([freq])
         outtable = pt.table(outfilename, readonly=False, ack=False)
         outtable.putkeywords({'coords': coords})
         outtable.done()
