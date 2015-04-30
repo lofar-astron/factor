901a729c457c61cb6d4f1763efe24635b6c94261
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-04-30 16:06:51 +0200

    Debug

--- factor/operations/facet_ops.py
+++ factor/operations/facet_ops.py
@@ -444,13 +444,11 @@ class FacetSelfcal(Operation):
         self.log.debug('Merging chunk instrument parmdbs...')
         merged_parmdb_phaseamp_amp1_mapfiles = []
         for i, chunks in enumerate(chunks_list):
-            self.log.debug('{0}'.format([chunk.parmdb_phaseamp_amp1 for chunk
-                in chunks]))
             merged_parmdb_phaseamp_amp1_mapfiles.append(self.write_mapfile(
                 [merge_chunk_parmdbs([chunk.parmdb_phaseamp_amp1 for chunk
                 in chunks], prefix='merged_chunks_amps1')], prefix='merged_amps1',
                 direction=d_list[i], host_list=d_hosts[i]))
-
+        0/0
         self.log.info('Smoothing amplitude solutions...')
         actions = [Smooth(self.parset, dm, p['smooth_amp1'], pm,
             prefix='facet_amp', direction=d, index=2)
