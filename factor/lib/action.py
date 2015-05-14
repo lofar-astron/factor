38559e2676665ed0e758d3937ea260136de2a497
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-05-14 20:28:21 +0200

    Add method to call pipeline directly

--- factor/lib/action.py
+++ factor/lib/action.py
@@ -219,6 +219,28 @@ class Action(object):
         return all_done
 
 
+    def call_generic_pipeline(self):
+        """
+        Creates a GenericPipeline object and runs the pipeline
+        """
+        from genericpipeline.bin import genericpipeline as gp
+        from factor.lib.context import RedirectStdStreams
+        import sys
+
+        sys.argv = [self.pipeline_executable, self.pipeline_parset_file, '-d',
+            '-c', self.pipeline_config_file]
+        with open("{0}.out.log".format(self.logbasename), "wb") as out, \
+           open("{0}.err.log".format(self.logbasename), "wb") as err:
+            with RedirectStdStreams(stdout=out, stderr=err):
+                status = gp.GenericPipeline().main()
+
+        if status == 0:
+            return self.get_results()
+        else:
+            self.log.error('Action did not run successfully. Exiting')
+            sys.exit(1)
+
+
     def run(self):
         """
         Runs the pipeline
--- factor/lib/context.py
+++ factor/lib/context.py
@@ -4,7 +4,7 @@ Definition of context managers (with statements) used for actions and operations
 import time
 import logging
 
-class Timer():
+class Timer(object):
     """
     Context manager used to time actions and operations
     """
@@ -35,3 +35,23 @@ class Timer():
         elapsed = (time.time() - self.start)
         self.log.debug('Time for {0}: {1} sec'.format(self.type, int(elapsed)))
 
+
+class RedirectStdStreams(object):
+    """
+    Context manager used to redirect streams
+    """
+    import sys
+
+    def __init__(self, stdout=None, stderr=None):
+        self._stdout = stdout or sys.stdout
+        self._stderr = stderr or sys.stderr
+
+    def __enter__(self):
+        self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
+        self.old_stdout.flush(); self.old_stderr.flush()
+        sys.stdout, sys.stderr = self._stdout, self._stderr
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._stdout.flush(); self._stderr.flush()
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
