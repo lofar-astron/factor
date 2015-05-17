546e1d50c0cc838050b316628e7cc8bb249ef571
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-05-17 13:26:45 +0200

    Add imports

--- factor/lib/context.py
+++ factor/lib/context.py
@@ -40,18 +40,19 @@ class RedirectStdStreams(object):
     """
     Context manager used to redirect streams
     """
-    import sys
-
     def __init__(self, stdout=None, stderr=None):
+        import sys
         self._stdout = stdout or sys.stdout
         self._stderr = stderr or sys.stderr
 
     def __enter__(self):
+        import sys
         self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
         self.old_stdout.flush(); self.old_stderr.flush()
         sys.stdout, sys.stderr = self._stdout, self._stderr
 
     def __exit__(self, exc_type, exc_value, traceback):
+        import sys
         self._stdout.flush(); self._stderr.flush()
         sys.stdout = self.old_stdout
         sys.stderr = self.old_stderr
