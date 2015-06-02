48cc13ae5a0ab2be7edd88ec372db9245b6b2769
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-06-02 15:20:42 +0200

    Fix plugin

--- factor/pipeline/plugins/PipelineStep_copyParmdbtoMS.py
+++ factor/pipeline/plugins/PipelineStep_copyParmdbtoMS.py
@@ -12,24 +12,16 @@ def plugin_main(args, **kwargs):
     parmdb_mapfile : str
         Filename of parmdb datamap
 
-    Returns
-    -------
-    result : dict
-        New parmdb datamap filename
-
     """
-    result = {}
-    ms_datamap = kwargs['ms_mapfile']
-    parmdb_datamap = kwargs['parmdb_mapfile']
+    ms_mapfile = kwargs['ms_mapfile']
+    parmdb_mapfile = kwargs['parmdb_mapfile']
+
+    ms_datamap = DataMap.load(mapfile)
+    parmdb_datamap = DataMap.load(mapfile)
+
     for ms, parmdb in zip(ms_datamap, parmdb_datamap):
         from_file = parmdb.file
         to_file = os.path.join(ms.file, 'instrument')
         if os.path.exists(to_file):
             os.system('rm -rf {0}'.format(to_file))
         os.system('cp -r {0} {1}'.format(from_file, to_file))
-        parmdb.file = to_file
-
-    out_mapfile = os.path.splitext(parmdb_mapfile)[0] + '_copyparmdbtoms.' + os.path.splitext(parmdb_mapfile)[1]
-    parmdb_datamap.save(out_mapfile)
-    result['mapfile'] = out_mapfile
-    return result
