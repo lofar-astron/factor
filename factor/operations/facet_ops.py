47ecc8625a56b05b75c95183a3af923215d32ea6
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-06-09 13:25:58 +0200

    Combine facet pipelines into one

--- factor/operations/facet_ops.py
+++ factor/operations/facet_ops.py
@@ -8,14 +8,8 @@ Classes
 -------
 FacetAdd : Operation
     Adds facet sources to data
-FacetSetup : Operation
-    Sets up the data for selfcal
 FacetSelfcal : Operation
-    Runs selfcal cycle on facet calibrator
-FacetImage : Operation
-    Images the entire facet
-FacetCheck : Operation
-    Subtracts all facet sources from data and images residuals
+    Runs the selfcal and imaging of a facet
 FacetSub : Operation
     Subtracts all facet sources from data
 FacetAddAllFinal : Operation
@@ -78,34 +72,6 @@ class FacetAdd(Operation):
             'dir_indep_instrument_parmdbs.datamap')
 
 
-class FacetSetup(Operation):
-    """
-    Operation to set up data for selfcal
-    """
-    def __init__(self, parset, bands, direction):
-        super(FacetSetup, self).__init__(parset, bands, direction,
-            name='FacetSetup')
-
-        # Define parameters needed for this operation
-        self.parms_dict = {'input_dir': parset['dir_ms'],
-                           'parset_dir': self.factor_parset_dir,
-                           'skymodel_dir': self.factor_skymodel_dir,
-                           'mapfile_dir': self.mapfile_dir,
-                           'pipeline_dir': self.factor_pipeline_dir,
-                           'shifted_cal_bands_datamap': self.direction.shifted_cal_bands_datamap,
-                           'dir_indep_parmdbs_datamap': self.direction.dir_indep_parmdbs_datamap,
-                           'hosts': self.direction.hosts}
-
-
-    def finalize(self):
-        """
-        Finalize this operation
-        """
-        # Add output datamap to direction object
-        self.direction.shifted_cal_concat_datamap = os.path.join(self.mapfile_dir,
-            'shifted_cal_concat.datamap')
-
-
 class FacetSelfcal(Operation):
     """
     Operation to selfcal one or more directions
@@ -121,9 +87,15 @@ class FacetSelfcal(Operation):
                            'mapfile_dir': self.mapfile_dir,
                            'pipeline_dir': self.factor_pipeline_dir,
                            'script_dir': self.factor_script_dir,
-                           'shifted_cal_concat_datamap': self.direction.shifted_cal_concat_datamap,
-                           'wplanes': self.direction.wplanes,
-                           'imsize': self.direction.cal_imsize,
+                           'shifted_cal_bands_datamap': self.direction.shifted_cal_bands_datamap,
+                           'shifted_all_bands_datamap': self.direction.shifted_all_bands_datamap,
+                           'shifted_empty_bands_datamap': self.direction.shifted_empty_bands_datamap,
+                           'dir_indep_parmdbs_datamap': self.direction.dir_indep_parmdbs_datamap,
+                           'field_ra': self.direction.field_ra,
+                           'field_dec': self.direction.field_dec,
+                           'facet_imsize': self.direction.facet_imsize,
+                           'cal_wplanes': self.direction.wplanes,
+                           'cal_imsize': self.direction.cal_imsize,
                            'chunk_width': self.direction.solint_a*2,
                            'solint_p': self.direction.solint_p,
                            'solint_a': self.direction.solint_a,
@@ -135,67 +107,12 @@ class FacetSelfcal(Operation):
         Finalize this operation
         """
         # Add output datamap to direction object
+        self.direction.shifted_cal_concat_datamap = os.path.join(self.mapfile_dir,
+            'shifted_cal_concat.datamap')
         self.direction.dir_dep_parmdb_datamap = os.path.join(self.mapfile_dir,
             'dir_dep_parmdb.datamap')
-
-
-class FacetImage(Operation):
-    """
-    Operation to image the full facet
-    """
-    def __init__(self, parset, bands, direction, name='FacetImage'):
-        super(FacetImage, self).__init__(parset, bands, direction,
-            name=name)
-
-        # Define parameters needed for this operation
-        self.parms_dict = {'input_dir': parset['dir_ms'],
-                           'parset_dir': self.factor_parset_dir,
-                           'skymodel_dir': self.factor_skymodel_dir,
-                           'mapfile_dir': self.mapfile_dir,
-                           'pipeline_dir': self.factor_pipeline_dir,
-                           'shifted_all_bands_datamap': self.direction.shifted_all_bands_datamap,
-                           'dir_dep_parmdb_datamap': self.direction.dir_dep_parmdb_datamap,
-                           'npix': self.direction.facet_imsize,
-                           'hosts': self.direction.hosts}
-
-
-    def finalize(self):
-        """
-        Finalize this operation
-        """
-        # Add output datamap to direction object
         self.direction.facet_image_mapfile = os.path.join(self.mapfile_dir,
             'facet_image.datamap')
-
-
-class FacetCheck(Operation):
-    """
-    Operation to subtract final facet sky model from facet visibilties
-    """
-    def __init__(self, parset, bands, direction):
-        super(FacetCheck, self).__init__(parset, bands, direction,
-            name='FacetCheck')
-
-        # Define parameters needed for this operation
-        self.parms_dict = {'input_dir': parset['dir_ms'],
-                           'parset_dir': self.factor_parset_dir,
-                           'skymodel_dir': self.factor_skymodel_dir,
-                           'mapfile_dir': self.mapfile_dir,
-                           'pipeline_dir': self.factor_pipeline_dir,
-                           'shifted_all_bands_datamap': self.direction.shifted_all_bands_datamap,
-                           'shifted_empty_bands_datamap': self.direction.shifted_empty_bands_datamap,
-                           'dir_indep_parmdbs_datamap': self.direction.dir_indep_parmdbs_datamap,
-                           'dir_dep_parmdb_datamap': self.direction.dir_dep_parmdb_datamap,
-                           'field_ra': self.direction.field_ra,
-                           'field_dec': self.direction.field_dec,
-                           'hosts': self.direction.hosts}
-
-
-    def finalize(self):
-        """
-        Finalize this operation
-        """
-        # Add check flag to direction object
         try:
             ok_datamap = DataMap.load(os.path.join(self.mapfile_dir,
                 'verify_subtract.datamap'))
