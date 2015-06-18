2a669ba66f0cafd1d4dc2a723cb1f98ff412b17f
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-06-18 16:39:22 +0200

    Add step to copy beam info

---
+++ factor/scripts/copy_beam_info.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+"""
+Script to copy beam info to WSClean MFS FITS images
+"""
+import argparse
+from argparse import RawTextHelpFormatter
+from astropy.io import fits as pyfits
+
+
+def main(image):
+    """
+    Copy the beam info to a WSClean MFS FITS image
+
+    This script assumes standard WSClean naming conventions
+
+    Parameters
+    ----------
+    image : str
+        Filename of image in which the beam info will be entered
+
+    """
+    templateim = image.replace('-MFS-', '-0000-')
+    hduimtemplate = pyfits.open(templateim)
+    hduim = pyfits.open(image, mode='update')
+    headertemplate = hduimtemplate[0].header
+    header = hduim[0].header
+    bmaj = headertemplate['BMAJ']
+    bmin = headertemplate['BMIN']
+    bpa = headertemplate['BPA']
+
+    header.update('BMAJ', bmaj, "")
+    header.update('BMIN', bmin, "")
+    header.update('BPA', bpa, "")
+
+    hduim.flush()
+    hduim.close()
+    hduimtemplate.close()
+
+
+if __name__ == '__main__':
+    descriptiontext = "Copy beam info.\n"
+
+    parser = argparse.ArgumentParser(description=descriptiontext, formatter_class=RawTextHelpFormatter)
+    parser.add_argument('image', help='filename of the WSClean MFS FITS image to copy to')
+    args = parser.parse_args()
+
+    main(args.image, args.templateim)
