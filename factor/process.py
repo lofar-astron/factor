0cc3f211f8afbf61bd318f44d17d33ee105bbd38
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-06-24 15:37:40 +0200

    Save direction state

--- factor/process.py
+++ factor/process.py
@@ -315,6 +315,7 @@ def run(parset_file, logging_level='info', dry_run=False, test_run=False):
         # Search for nearest direction with successful selfcal
         nearest = factor.directions.find_nearest(d, dirs_with_selfcal)
         d.dir_dep_parmdb_datamap = nearest.dir_dep_parmdb_datamap
+        d.save_state()
     dirs_to_image.extend(dirs_to_transfer)
 
     if len(dirs_to_image) > 0:
