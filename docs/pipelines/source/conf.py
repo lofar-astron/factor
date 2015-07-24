eaacf3bcc5c6a43835e02222c2c241c511570ff4
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-07-24 10:18:06 +0200

    Fixed formatting

--- docs/pipelines/source/conf.py
+++ docs/pipelines/source/conf.py
@@ -124,12 +124,12 @@ html_theme = 'classic'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = facets.png
+html_logo = 'facets.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = facets_small.png
+html_favicon = 'facets_small.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
