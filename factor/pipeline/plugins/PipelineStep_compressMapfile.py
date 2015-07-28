4961b3953f5c8d9452c43a4196862080480ac961
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-07-28 10:37:18 +0200

    Removed virtual conat before wsclean calls

--- factor/pipeline/plugins/PipelineStep_compressMapfile.py
+++ factor/pipeline/plugins/PipelineStep_compressMapfile.py
@@ -15,6 +15,10 @@ def plugin_main(args, **kwargs):
         Directory for output mapfile
     filename: str
         Name of output mapfile
+    list_format : bool, optional
+        If True, the compreseed item will use a Python list format (e.g.,
+        '[file1, file2, ...]'. If False, it will be a space-separated list (e.g.,
+        'file1 file2 ...'
 
     Returns
     -------
@@ -25,12 +29,24 @@ def plugin_main(args, **kwargs):
     mapfile_in = kwargs['mapfile_in']
     mapfile_dir = kwargs['mapfile_dir']
     filename = kwargs['filename']
+    if 'list_format' in kwargs:
+        list_format = kwargs['list_format']
+    else:
+        list_format = True
+    if type(list_format) is str:
+        if list_format.lower() == 'true':
+            list_format = True
+        else:
+            list_format = False
 
     map_in = DataMap.load(mapfile_in)
     map_out = DataMap([])
 
     map_in.iterator = DataMap.SkipIterator
-    newlist = '[{0}]'.format(','.join([item.file for item in map_in]))
+    if list_format:
+        newlist = '[{0}]'.format(','.join([item.file for item in map_in]))
+    else:
+        newlist = '{0}'.format(' '.join([item.file for item in map_in]))
     hosts = [item.host for item in map_in]
     map_out.data.append(DataProduct(hosts[0], newlist, False))
 
