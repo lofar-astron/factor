Fixed cal flux units

factor/directions.py
@@ -40,7 +40,7 @@ def directions_read(directions_file, factor_working_dir):
         types = np.dtype({'names': ['name', 'radec', 'atrous_do', 'mscale_field_do',
             'cal_imsize', 'solint_p', 'solint_a', 'field_imsize', 'dynamic_range',
             'region_selfcal', 'region_field', 'peel_skymodel', 'outlier_source',
-            'cal_size_deg', 'cal_flux_jy'], 'formats':['S255', 'S255', 'S5',
+            'cal_size_deg', 'cal_flux_mjy'], 'formats':['S255', 'S255', 'S5',
             'S5', int, int, int, int, 'S2', 'S255', 'S255', 'S255',
             'S5', float, float]})
         directions = np.genfromtxt(directions_file, comments='#', dtype=types)
@@ -97,10 +97,10 @@ def directions_read(directions_file, factor_working_dir):
                 cal_size_deg = None
             else:
                 cal_size_deg = direction['cal_size_deg']
-            if np.isnan(direction['cal_flux_jy']):
+            if np.isnan(direction['cal_flux_mjy']):
                 cal_flux_jy = None
             else:
-                cal_flux_jy = direction['cal_flux_jy']
+                cal_flux_jy = direction['cal_flux_mjy'] / 1000.0
         else:
             cal_size_deg = None
             cal_flux_jy = None