cfce20bf97f4a8bae10f096442a0f210287625b4
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-10-05 13:30:33 +0200

    Add quick check for missing bands

--- factor/pipeline/plugins/PipelineStep_compressMapfile.py
+++ factor/pipeline/plugins/PipelineStep_compressMapfile.py
@@ -41,12 +41,42 @@ def plugin_main(args, **kwargs):
 
     map_in = DataMap.load(mapfile_in)
     map_out = DataMap([])
-
     map_in.iterator = DataMap.SkipIterator
+
+    # Try to detect missing bands by common naming conventions:
+    # SB010 or B10
+    # This assumes bands are ordered from low to high
+    band_numbers = []
+    file_list = []
+    skip_check = False
+    for item in map_in:
+        try:
+            if 'SB' in item.file:
+                band_indx = int(item.file.split('SB')[1][0:3])
+            elif 'B' in item.file:
+                band_indx = int(item.file.split('B')[1][0:2])
+            if i == 0:
+                start_indx = band_indx
+            band_numbers.append(band_indx)
+            file_list.append(item.file)
+        except ValueError:
+            file_list = [item.file for item in map_in]
+            skip_check = True
+            break
+    if not skip_check:
+        indx = 0
+        for i in range(len(band_numbers)):
+            if band_numbers[i] != indx+start_indx:
+                file_list.insert(indx, 'dummy.ms')
+                indx += 1
+            indx += 1
+
     if list_format:
-        newlist = '[{0}]'.format(','.join([item.file for item in map_in]))
+        newlist = '[{0}]'.format(','.join(file_list))
     else:
-        newlist = '{0}'.format(' '.join([item.file for item in map_in]))
+        newlist = '{0}'.format(' '.join(file_list))
+
+    # Just assign host of first file to compressed file
     hosts = [item.host for item in map_in]
     map_out.data.append(DataProduct(hosts[0], newlist, False))
 
