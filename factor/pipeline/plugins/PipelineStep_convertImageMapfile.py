d7fafe951b6a7d991a84f8da74852bc2e56d1c67
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-10-15 11:57:03 +0200

    Add mapfile adjustment steps

---
+++ factor/pipeline/plugins/PipelineStep_convertImageMapfile.py
@@ -0,0 +1,42 @@
+import os
+from lofarpipe.support.data_map import DataMap
+from lofarpipe.support.data_map import DataProduct
+
+
+def plugin_main(args, **kwargs):
+    """
+    Alters the image filenames in a mapfile to match previous loop
+
+    Parameters
+    ----------
+    mapfile_in : str
+        Filename of datamap to trim
+    counter : int
+        Loop counter
+    mapfile_dir : str
+        Directory for output mapfile
+    filename: str
+        Name of output mapfile
+
+    Returns
+    -------
+    result : dict
+        New datamap filename
+
+    """
+    mapfile_in = kwargs['mapfile_in']
+    counter = int(kwargs['counter'])
+    mapfile_dir = kwargs['mapfile_dir']
+    filename = kwargs['filename']
+
+    map_out = DataMap([])
+    map_in = DataMap.load(mapfile_in)
+
+    for i, item in enumerate(map_in):
+        map_out.data.append(DataProduct(item.host, item.file, item.skip))
+
+    fileid = os.path.join(mapfile_dir, filename)
+    map_out.save(fileid)
+    result = {'mapfile': fileid}
+
+    return result
