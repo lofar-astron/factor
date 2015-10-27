4206c0a54e7fa7f748ae1ecbbeed39024440e2a3
David Rafferty <drafferty@hs.uni-hamburg.de>
2015-10-27 10:07:10 +0100

    Add initial version of pre-averaging script

---
+++ factor/scripts/pre_average.py
@@ -0,0 +1,295 @@
+#! /usr/bin/env python
+"""
+Script to pre-average data using a sliding Gaussian kernel on the weights
+"""
+import argparse
+from argparse import RawTextHelpFormatter
+import numpy as np
+import glob
+import sys
+import os
+import itertools
+from scipy.ndimage.filters import gaussian_filter1d as gfilter
+import pyrap.tables as pt
+import acor
+from astropy.stats import median_absolute_deviation
+
+
+def main(ms_file, parmdb_file, input_colname, output_colname, minutes_per_block=10.0,
+    verbose=True):
+    """
+    Pre-average data using a sliding Gaussian kernel on the weights
+    """
+    baseline_dict = get_baseline_lengths(ms_file)
+
+    # Iterate through time chunks
+    tab = pt.table(self.file, ack=False)
+    remaining_time = tab.col('TIME')[-1] - tab.col('TIME')[0] # seconds
+    tab.close()
+    t_delta = minutes_per_block * 60.0 # seconds
+    t1 = 0.0
+    while remaining_time > 0.0:
+        if remaining_time < 1.5 * t_delta:
+            # If remaining time is too short, just include it all in this chunk
+            t_delta = remaining_time + 10.0
+        remaining_time -= t_delta
+
+        # Find ionfactor for this period
+        ionfactor = find_ionfactor(parmdb_file, baseline_dict, t1, t1+t_delta)
+
+        # Do pre-averaging for this period
+        BLavg(msfile, baseline_dict, input_colname, output_colname, ionfactor,
+            t1, t1+t_delta)
+
+        t1 += t_delta
+
+
+def get_baseline_lengths(ms_file):
+    """
+    Returns dict of baseline lengths for all baselines in input dataset
+    """
+    t = pt.table(ms_file, ack=False)
+    anttab = pt.table(ms_file+'::ANTENNA')
+    antnames = anttab.getcol('NAME')
+    anttab.close()
+    all_uvw = t.getcol('UVW')
+    baseline_dict = {}
+    for ant in itertools.product(set(ant1), set(ant2)):
+        if ant[0] >= ant[1]: continue
+        sel1 = np.where(ant1 == ant[0])[0]
+        sel2 = np.where(ant2 == ant[1])[0]
+        sel = sorted(list(frozenset(sel1).intersection(sel2)))
+        uvw = all_uvw[sel, :]
+        uvw_dist = np.sqrt(uvw[:, 0]**2 + uvw[:, 1]**2 + uvw[:, 2]**2)
+        baseline_dict['{0}'.format(ant[0])] = antnames[ant[0]]
+        baseline_dict['{0}'.format(ant[1])] = antnames[ant[1]]
+        baseline_dict['{0}-{1}'.format(ant[0], ant[1])] = np.mean(uvw_dist) / 1.e3
+    t.close()
+
+    return baseline_dict
+
+
+def find_ionfactor(parmdb_file, baseline_dict, t1, t2):
+    """
+    Finds ionospheric scaling factor
+    """
+    pdb_in = lofar.parmdb.parmdb(stations_ms)
+    parms = pdb_in.getValuesGrid('*')
+
+    # Filter any stations not in both the instrument table and the ms
+    stations_pbd = set([s.split(':')[-1] for s in pdb_in.getNames()])
+    stations_ms = set([s for s in baseline_dict.itervalues() if type(s) is str])
+    stations = list(stations_pbd.intersection(stations_ms))
+
+    # Select long baselines only, as they will set the ionfactor scaling
+    ant1 = []
+    ant2 = []
+    dist = []
+    min_length = 2.e3
+    for k, v in baseline_dict.iteritems():
+        if v > min_length:
+            s1 = stations_ms[k.split('-')[0]]
+            s2 = stations_ms[k.split('-')[1]]
+            if s1 in stations and s2 in stations:
+                ant1.append(s1)
+                ant2.append(s2)
+                dist.append(v)
+
+    # Find correlation times
+    target_rms_rad = 0.2
+    rmstimes = []
+    freq = None
+    timepersolution = None
+    for a1, a2, d in zip(ant1, ant2, dist):
+        if freq is None:
+            freq = np.copy(parms['Gain:0:0:Phase:{}'.format(a1)]['freqs'])[0]
+        if timepersolution is None:
+            timepersolution = np.copy(parms['Gain:0:0:Phase:{}'.format(a1)]['timewidths'])[0]
+        ph1 = np.copy(parms['Gain:0:0:Phase:{}'.format(a1)]['values'])
+        ph2 = np.copy(parms['Gain:0:0:Phase:{}'.format(a2)]['values'])
+
+        print('BL: {0}-{1}'.format(ant1, ant2))
+        rmstime = None
+        ph = unwrap_fft(ph2 - ph1)
+
+        step = 1
+        for i in range(1, len(ph)/2, step):
+            p1 = phpart[i:]
+            p2 = phpart[:-i]
+            rms = np.linalg.norm(p1-p2) / np.sqrt(len(p1))
+            mad = median_absolute_deviation(p1-p2)
+            mean = np.mean(p1-p2)
+            if rms + mean > target_rms_rad:
+                rmstime = i
+                break
+        if rmstime is None:
+            rmstime = len(ph)/2
+        rmstimes.append(rmstime)
+
+    # Find the mean ionfactor assuming that the correlation time goes as
+    # t_corr ~ 1/sqrt(BL). The ionfactor is defined in BLavg() as:
+    #
+    #     ionfactor = t_corr / ( np.sqrt((25.e3 / dist)) * (freq / 60.e6) )
+    #
+    ionfactor = np.mean(
+        np.array(rmstimes) /
+        np.sqrt(25.e3 / np.array(dist)) *
+        freq / 60e6
+        ) * timepersolution # sec
+
+    return ionfactor
+
+
+def BLavg(msfile, baseline_dict, input_colname, output_colname, ionfactor, t1,
+    t2, clobber=True):
+    """
+    Averages data using a sliding Gaussian kernel on the weights
+    """
+    if not os.path.exists(msfile):
+        logging.error("Cannot find MS file.")
+        sys.exit(1)
+
+    # prepare new ms
+    if not options.overwrite:
+        msfile_new = msfile.replace('.MS','-BLavg.MS')
+        if os.path.exists(msfile_new):
+            if not options.clobber:
+                logging.error("Output file exists and clobber=False")
+                sys.exit(1)
+            os.system('rm -r '+msfile_new)
+        logging.info("Copying MS, this may take a while.")
+        os.system('cp -r '+msfile+' '+msfile_new)
+        msfile = msfile_new
+
+    # open input/output MS
+    t = pt.table(msfile, readonly=False, ack=False)
+    freqtab = pt.table(msfile + '::SPECTRAL_WINDOW', ack=False)
+    freq = freqtab.getcol('REF_FREQUENCY')
+    freqtab.close()
+    wav = 299792458. / freq
+    timepersample = t.getcell('INTERVAL',0)
+    starttime = t[0]['TIME']
+    ms = t.query('TIME >= ' + str(starttime+t1) + ' && '
+      'TIME < ' + str(starttime+t2), sortlist='TIME,ANTENNA1,ANTENNA2')
+    all_time = ms.getcol('TIME_CENTROID')
+
+    ant1 = ms.getcol('ANTENNA1')
+    ant2 = ms.getcol('ANTENNA2')
+    all_data = ms.getcol(options.column)
+    all_weights = ms.getcol('WEIGHT_SPECTRUM')
+    all_flags = ms.getcol('FLAG')
+    all_uvw = ms.getcol('UVW')
+
+    # iteration on baseline combination
+    for ant in itertools.product(set(ant1), set(ant2)):
+
+        if ant[0] >= ant[1]: continue
+        sel1 = np.where(ant1 == ant[0])[0]
+        sel2 = np.where(ant2 == ant[1])[0]
+        sel = sorted(list(frozenset(sel1).intersection(sel2)))
+
+        # compute the FWHM
+        uvw = all_uvw[sel,:]
+        uvw_dist = np.sqrt(uvw[:, 0]**2 + uvw[:, 1]**2 + uvw[:, 2]**2)
+        dist = np.mean(uvw_dist) / 1.e3
+        stddev = ionfactor * np.sqrt((25.e3 / dist)) * (freq / 60.e6) # in sec
+        stddev = stddev/timepersample # in samples
+        logging.debug("For BL %i - %i (dist = %.1f km): sigma=%.2f samples." % (ant[0], ant[1], dist, stddev))
+
+        #    Multiply every element of the data by the weights, convolve both
+        #    the scaled data and the weights, and then divide the convolved data
+        #    by the convolved weights (translating flagged data into weight=0).
+        #    That's basically the equivalent of a running weighted average with
+        #    a Gaussian window function.
+
+        # get weights
+        flags = all_flags[sel,:,:]
+        weights = all_weights[sel,:,:]*~flags # set flagged data weight to 0
+        # get data
+        data = all_data[sel,:,:]*weights
+
+        # smear weighted data and weights
+        dataR = gfilter(np.real(data), stddev, axis=0)#, truncate=4.)
+        dataI = gfilter(np.imag(data), stddev, axis=0)#, truncate=4.)
+        weights = gfilter(weights, stddev, axis=0)#, truncate=4.)
+
+        # re-create data
+        # NOTE: both data and/or weight might be 0
+        d = (dataR + 1j * dataI)
+        weights[np.where(data == 0)] = np.nan # prevent 0/0 exception
+        d = d/weights # when weights == 0 or nan -> data is nan
+        # if data is nan, put data to 0 (anyway they must be flagged)
+        all_data[sel,:,:] = np.nan_to_num(d)
+        all_weights[sel,:,:] = np.nan_to_num(weights)
+
+    ms.putcol('DATA', all_data)
+    ms.putcol('WEIGHT_SPECTRUM', all_weights)
+    ms.close()
+
+
+def smooth(x, window_len=10, window='hanning'):
+    """smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal
+        window_len: the dimension of the smoothing window
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    import numpy as np
+    t = np.linspace(-2,2,0.1)
+    x = np.sin(t)+np.random.randn(len(t))*0.1
+    y = smooth(x)
+
+    see also:
+
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+    if window_len < 3:
+        return x
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+    s=np.r_[2*x[0]-x[window_len:1:-1], x, 2*x[-1]-x[-1:-window_len:-1]]
+    #print(len(s))
+
+    if window == 'flat': #moving average
+        w = np.ones(window_len,'d')
+    else:
+        w = getattr(np, window)(window_len)
+    y = np.convolve(w/w.sum(), s, mode='same')
+    return y[window_len-1:-window_len+1]
+
+
+if __name__ == '__main__':
+    descriptiontext = "Pre-average data.\n"
+
+    parser = argparse.ArgumentParser(description=descriptiontext, formatter_class=RawTextHelpFormatter)
+    parser.add_argument('ms_file', help='Filename of input dataset')
+    parser.add_argument('parmdb_file', help='Filename of input direction-independent selfcal instrument parmdb')
+    parser.add_argument('input_colname', help='Name of input column to pre-average')
+    parser.add_argument('output_colname', help='Name of output column')
+    args = parser.parse_args()
+
+    main(args.ms_file, args.parmdb_file, args.input_colname, args.output_colname)
