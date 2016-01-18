57007341527b1e1134e569f765f9c9bc5c4d66b5
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-01-18 14:27:36 +0100

    Try axis instead

--- factor/check_progress.py
+++ factor/check_progress.py
@@ -143,9 +143,8 @@ def plot_state(directions_list):
     selfcal_ok_patch = Patch(color='g', label='Facetselfcal complete')
     selfcal_not_ok_patch = Patch(color='r', label='Facetsefcal failed')
     image_ok_patch = Patch(color='b', label='Facetimage complete')
-    plt.legend(handles=[not_processed_patch, selfcal_ok_patch, image_ok_patch,
-        selfcal_not_ok_patch], labels=['Unprocessed', 'Facetselfcal complete',
-        'Facetsefcal failed', 'Facetimage complete'])
+    ax.legend(handles=[not_processed_patch, selfcal_ok_patch, image_ok_patch,
+        selfcal_not_ok_patch])
 
     # Add check for mouse clicks
     fig.canvas.mpl_connect('pick_event', on_pick)
