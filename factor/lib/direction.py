8e430f3a93dc126a60102ca31561c0294acb6970
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-01-28 16:02:06 +0100

    Check for imsize

--- factor/lib/direction.py
+++ factor/lib/direction.py
@@ -456,21 +456,23 @@ class Direction(object):
 
         # For selfcal, average to 2 MHz per channel and 120 s per time slot for
         # an image of 512 pixels
-        target_bandwidth_mhz = 2.0 * 512.0 / self.cal_imsize
-        target_timewidth_s = 120 * 512.0 / self.cal_imsize # used for imaging only
-        self.facetselfcal_freqstep = max(1, min(int(round(target_bandwidth_mhz * 1e6 / chan_width_hz)), nchan))
-        while nchan % self.facetselfcal_freqstep:
-            self.facetselfcal_freqstep += 1
-        self.facetselfcal_timestep = max(1, int(round(target_timewidth_s / timestep_sec)))
+        if self.cal_imsize is not None:
+            target_bandwidth_mhz = 2.0 * 512.0 / self.cal_imsize
+            target_timewidth_s = 120 * 512.0 / self.cal_imsize # used for imaging only
+            self.facetselfcal_freqstep = max(1, min(int(round(target_bandwidth_mhz * 1e6 / chan_width_hz)), nchan))
+            while nchan % self.facetselfcal_freqstep:
+                self.facetselfcal_freqstep += 1
+            self.facetselfcal_timestep = max(1, int(round(target_timewidth_s / timestep_sec)))
 
         # For facet imaging, average to 0.5 MHz per channel and 30 sec per time
         # slot for an image of 2048 pixels
-        target_bandwidth_mhz = 0.5 * 2048.0 / self.facet_imsize
-        target_timewidth_s = 30 * 2048.0 / self.facet_imsize
-        self.facetimage_freqstep = max(1, min(int(round(target_bandwidth_mhz * 1e6 / chan_width_hz)), nchan))
-        while nchan % self.facetimage_freqstep:
-            self.facetimage_freqstep += 1
-        self.facetimage_timestep = max(1, int(round(target_timewidth_s / timestep_sec)))
+        if self.facet_imsize is not None:
+            target_bandwidth_mhz = 0.5 * 2048.0 / self.facet_imsize
+            target_timewidth_s = 30 * 2048.0 / self.facet_imsize
+            self.facetimage_freqstep = max(1, min(int(round(target_bandwidth_mhz * 1e6 / chan_width_hz)), nchan))
+            while nchan % self.facetimage_freqstep:
+                self.facetimage_freqstep += 1
+            self.facetimage_timestep = max(1, int(round(target_timewidth_s / timestep_sec)))
 
         # For selfcal verify, average to 2 MHz per channel and 60 sec per time
         # slot
