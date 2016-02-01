075332a8a855058e4e4a3d28a5a9e90e1919f641
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-02-01 20:29:35 +0100

    Fix typo

--- factor/check_progress.py
+++ factor/check_progress.py
@@ -53,7 +53,7 @@ def run(parset_file, trim_names=True):
     log.setLevel(logging.ERROR)
     all_directions = load_directions(parset_file)
     if len(all_directions) == 0:
-        log.error('No directions found. Please check parset")
+        log.error('No directions found. Please check parset')
         sys.exit(1)
     plot_state(all_directions, trim_names=trim_names)
 
