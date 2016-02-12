febb23a339883f6d71d8deebc0800b60f3f3cff0
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-02-12 13:50:38 +0100

    Fix blanking of patch models

--- factor/scripts/blank_image.py
+++ factor/scripts/blank_image.py
@@ -20,8 +20,8 @@ def read_vertices(filename):
     return direction_dict['vertices']
 
 
-def main(input_image_file, vertices_file, output_image_file, blank_value='zeros',
-    img_format='fits'):
+def main(input_image_file, vertices_file, output_image_file, blank_value='zero',
+    img_format='fits', image_is_casa_model=False, nterms=1):
     """
     Blank a region in an image
 
@@ -37,54 +37,90 @@ def main(input_image_file, vertices_file, output_image_file, blank_value='zeros'
     output_image_file : str
         Filename of output image
     blank_value : str, optional
-        Value for blanks (one of 'zeros' or 'nans')
+        Value for blanks (one of 'zero' or 'nan')
     img_format : str, optional
         Format of output mask image (one of 'fits' or 'casa')
+    image_is_casa_model : bool, optional
+        If True, the input and output image files are treated as the root name
+        of a casa model image (or images)
+    nterms : int, optional
+        If image_is_casa_model is True, this argument sets the number of nterms
+        for the model
 
     """
-    im = pim.image(input_image_file)
-    data = im.getdata()
-    coordsys = im.coordinates()
-    imshape = im.shape()
-    new_im = pim.image('', shape=imshape, coordsys=coordsys)
-
-    # Construct polygon
-    vertices = read_vertices(vertices_file)
-    RAverts = vertices[0]
-    Decverts = vertices[1]
-    xvert = []
-    yvert = []
-    for RAvert, Decvert in zip(RAverts, Decverts):
-        pixels = new_im.topixel([0, 1, Decvert*np.pi/180.0,
-            RAvert*np.pi/180.0])
-        xvert.append(pixels[2]) # x -> Dec
-        yvert.append(pixels[3]) # y -> RA
-    poly = Polygon(xvert, yvert)
-
-    # Find distance to nearest poly edge and blank those that
-    # are outside the facet (dist < 0)
-    pix_ind = np.indices(data[0, 0].shape)
-    dist = poly.is_inside(pix_ind[0], pix_ind[1])
-    outside_ind = np.where(dist < 0.0)
-    if len(outside_ind[0]) > 0:
-        if blank_value == 'zeros':
-            blank_val = 0.0
-        elif blank_value == 'nans':
-            blank_val = np.nan
+    if type(image_is_casa_model) is str:
+        if image_is_casa_model.lower() == 'true':
+            image_is_casa_model = True
         else:
-            print('Blank value type "{}" not understood.'.format(blank_with))
-            sys.exit(1)
-        data[0, 0, pix_ind[0][outside_ind], pix_ind[1][outside_ind]] = blank_val
+            image_is_casa_model = False
 
-    # Save changes
-    new_im.putdata(data)
-    if img_format == 'fits':
-        new_im.tofits(output_image_file, overwrite=True)
-    elif img_format == 'casa':
-        new_im.saveas(output_image_file, overwrite=True)
+    if type(nterms) is str:
+        nterms = int(nterms)
+
+    if image_is_casa_model:
+        if ntermsi == 1:
+            input_image_files = [input_image_file+'.model']
+            output_image_files = [output_image_file+'.model']
+        if ntermsi == 2:
+            input_image_files = [input_image_file+'.model.tt0',
+                input_image_file+'.model.tt1']
+            output_image_files = [output_image_files+'.model.tt0',
+                output_image_files+'.model.tt1']
+        if ntermsi == 3:
+            input_image_files = [input_image_file+'.model.tt0',
+                input_image_file+'.model.tt1', input_image_file+'.model.tt2']
+            output_image_files = [output_image_files+'.model.tt0',
+                output_image_files+'.model.tt1', output_image_files+'.model.tt2']
     else:
-        print('Output image format "{}" not understood.'.format(img_format))
-        sys.exit(1)
+        input_image_files = [input_image_file]
+        output_image_files = [output_image_file]
+
+    for input_image, output_image in zip(input_image_files, output_image_files):
+        im = pim.image(input_image)
+        data = im.getdata()
+        coordsys = im.coordinates()
+        imshape = im.shape()
+        new_im = pim.image('', shape=imshape, coordsys=coordsys)
+
+        # Construct polygon
+        vertices = read_vertices(vertices_file)
+        RAverts = vertices[0]
+        Decverts = vertices[1]
+        xvert = []
+        yvert = []
+        for RAvert, Decvert in zip(RAverts, Decverts):
+            pixels = new_im.topixel([0, 1, Decvert*np.pi/180.0,
+                RAvert*np.pi/180.0])
+            xvert.append(pixels[2]) # x -> Dec
+            yvert.append(pixels[3]) # y -> RA
+        poly = Polygon(xvert, yvert)
+
+        # Find distance to nearest poly edge and blank those that
+        # are outside the facet (dist < 0)
+        pix_ind = np.indices(data[0, 0].shape)
+        dist = poly.is_inside(pix_ind[0], pix_ind[1])
+        outside_ind = np.where(dist < 0.0)
+        if len(outside_ind[0]) > 0:
+            if blank_value == 'zero':
+                blank_val = 0.0
+            elif blank_value == 'nan':
+                blank_val = np.nan
+            else:
+                print('Blank value type "{}" not understood.'.format(blank_with))
+                sys.exit(1)
+            data[0, 0, pix_ind[0][outside_ind], pix_ind[1][outside_ind]] = blank_val
+
+        # Save changes
+        new_im.putdata(data)
+        if image_is_casa_model:
+
+        if img_format == 'fits':
+            new_im.tofits(output_image, overwrite=True)
+        elif img_format == 'casa':
+            new_im.saveas(output_image, overwrite=True)
+        else:
+            print('Output image format "{}" not understood.'.format(img_format))
+            sys.exit(1)
 
 
 if __name__ == '__main__':
