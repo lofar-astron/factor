25aa80a822d05a4d898fd23a510a647c7413a8f7
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-02-12 13:53:13 +0100

    Fix variable name

--- factor/scripts/blank_image.py
+++ factor/scripts/blank_image.py
@@ -58,15 +58,15 @@ def main(input_image_file, vertices_file, output_image_file, blank_value='zero',
         nterms = int(nterms)
 
     if image_is_casa_model:
-        if ntermsi == 1:
+        if nterms == 1:
             input_image_files = [input_image_file+'.model']
             output_image_files = [output_image_file+'.model']
-        if ntermsi == 2:
+        if nterms == 2:
             input_image_files = [input_image_file+'.model.tt0',
                 input_image_file+'.model.tt1']
             output_image_files = [output_image_files+'.model.tt0',
                 output_image_files+'.model.tt1']
-        if ntermsi == 3:
+        if nterms == 3:
             input_image_files = [input_image_file+'.model.tt0',
                 input_image_file+'.model.tt1', input_image_file+'.model.tt2']
             output_image_files = [output_image_files+'.model.tt0',
