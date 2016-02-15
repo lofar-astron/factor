8ba404885cecb3c4c9113c32e92b4b5f1a4add26
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-02-15 09:51:56 +0100

    Tweak logging

--- factor/check_progress.py
+++ factor/check_progress.py
@@ -42,11 +42,19 @@ def run(parset_file, trim_names=True):
     """
     global all_directions
 
+    # Set logging level to ERROR to suppress extraneous info from parset read
+    logging.root.setLevel(logging.ERROR)
+    log.setLevel(logging.ERROR)
+
     # Read in parset and get directions
     all_directions = load_directions(parset_file)
     if len(all_directions) == 0:
         log.error('No directions found. Please check parset')
         sys.exit(1)
+
+    # Set logging level to normal
+    logging.root.setLevel(logging.INFO)
+    log.setLevel(logging.INFO)
 
     # Plot field
     log.info('Plotting facets...')
@@ -55,10 +63,6 @@ def run(parset_file, trim_names=True):
     log.info('Right-click on a facet to display its selfcal solutions and images')
     log.info('(In all cases, pan/zoom mode must be off)')
     log.info('Press "u" to update display (display is updated automatically every minute)')
-
-    # Set logging level to ERROR to suppress extraneous info
-    logging.root.setLevel(logging.ERROR)
-    log.setLevel(logging.ERROR)
 
     plot_state(all_directions, trim_names=trim_names)
 
