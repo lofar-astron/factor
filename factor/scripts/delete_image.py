a311a9183809bdec3b8bb0edc21de3745cf0d02c
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-03-04 12:55:00 +0100

    Fix search pattern

--- factor/scripts/delete_image.py
+++ factor/scripts/delete_image.py
@@ -14,7 +14,7 @@ def main(imageroot, counter, indx):
     """
     Delete images with given root name
 
-    The root name is assumed to contain the string "image?2." (e.g., "image42.").
+    The root name is assumed to contain the string "image?2" (e.g., "image42").
 
     Deletion is only done when counter > 0 (i.e., after the first selfcal pass).
 
@@ -36,10 +36,10 @@ def main(imageroot, counter, indx):
     # script with an existing mapfile from a previous imaging step. Therefore,
     # we find the root index in imageroot and replace it with the current one
     for i in range(5):
-        if imageroot.find('image{}2.'.format(i)) >= 0:
+        if imageroot.find('image{}2'.format(i)) >= 0:
             root_indx = i
             break
-    imageroot.replace('image{}2.'.format(root_indx), 'image{}2.'.format(indx))
+    imageroot.replace('image{}2'.format(root_indx), 'image{}2'.format(indx))
 
     if counter > 0:
         # First delete the "image?2" images
@@ -50,7 +50,7 @@ def main(imageroot, counter, indx):
                     shutil.rmtree(image)
 
         # Next delete the "image?1" images
-        images2  = glob.glob(imageroot.replace('image{}2.'.format(indx), 'image{}1.'.format(indx)) + '.*')
+        images2  = glob.glob(imageroot.replace('image{}2'.format(indx), 'image{}1'.format(indx)) + '.*')
         for image in images2:
             if os.path.exists(image):
                 if os.path.isdir(image):
