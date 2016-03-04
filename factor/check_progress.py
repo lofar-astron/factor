8aacdbfa314b93bc17cb38128883cab954caa14b
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-03-04 14:47:57 +0100

    Update to work with multi-scale selfcal

--- factor/check_progress.py
+++ factor/check_progress.py
@@ -460,10 +460,14 @@ def find_selfcal_images(direction):
     selfcal_dir = os.path.join(direction.working_dir, 'results', 'facetselfcal',
         direction.name)
     if os.path.exists(selfcal_dir):
-        selfcal_images = glob.glob(selfcal_dir+'/*.casa_image[0123]2.image.tt0')
+        selfcal_images = glob.glob(selfcal_dir+'/*.casa_image[01]2.image.tt0')
+        selfcal_images += glob.glob(selfcal_dir+'/*.casa_image22_iter*.image.tt0')
+        selfcal_images += glob.glob(selfcal_dir+'/*.casa_image[3]2.image.tt0')
         selfcal_images += glob.glob(selfcal_dir+'/*.casa_image42_iter*.image.tt0')
         if len(selfcal_images) == 0:
-            selfcal_images = glob.glob(selfcal_dir+'/*.casa_image[0123]2.image')
+            selfcal_images = glob.glob(selfcal_dir+'/*.casa_image[01]2.image')
+            selfcal_images += glob.glob(selfcal_dir+'/*.casa_image22_iter*.image')
+            selfcal_images += glob.glob(selfcal_dir+'/*.casa_image[3]2.image')
             selfcal_images += glob.glob(selfcal_dir+'/*.casa_image42_iter*.image')
         selfcal_images.sort()
     else:
