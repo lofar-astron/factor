a9ba04bcae2383c80507b1ba0b91c7d283d146d1
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-04-06 11:10:46 +0200

    Add reset for peeling operations

--- factor/process.py
+++ factor/process.py
@@ -88,6 +88,10 @@ def run(parset_file, logging_level='info', dry_run=False, test_run=False,
 
         # Do the peeling
         for d in peel_directions:
+            # Reset if needed. Reset of facetsub for peeled calibrators is not
+            # currently supported
+            d.reset_state(['outlierpeel', 'facetpeel', 'facetimage'])
+
             if d.is_outlier:
                 op = OutlierPeel(parset, bands, d)
             else:
