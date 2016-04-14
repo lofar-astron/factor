9ec2ab486a836c26d44d32166c66c26a4436c7c9
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-04-14 11:41:14 +0200

    Remove deprectation warnings

--- factor/parset.py
+++ factor/parset.py
@@ -283,12 +283,10 @@ def get_global_options(parset):
                 'parset but is not a valid global option'.format(option))
         if option in deprecated_options_imaging:
             log.warning('Option "{}" was given in the [global] section of the '
-                'parset but should be in the [imaging] section'.format(option),
-                DeprecationWarning)
+                'parset but should be in the [imaging] section'.format(option))
         if option in deprecated_options_cal:
             log.warning('Option "{}" was given in the [global] section of the '
-                'parset but should be in the [calibration] section'.format(option),
-                DeprecationWarning)
+                'parset but should be in the [calibration] section'.format(option))
 
     return parset_dict
 
