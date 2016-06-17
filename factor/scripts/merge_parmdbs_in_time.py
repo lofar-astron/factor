96ea298d7072aba80af2aca55ec3051e2f90331c
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-06-17 10:49:50 +0200

    Add check for overlapping parmdb entries

--- factor/scripts/merge_parmdbs_in_time.py
+++ factor/scripts/merge_parmdbs_in_time.py
@@ -34,6 +34,18 @@ def main(input_mslist, parmdb_name, outparmdb, clobber=True):
         input_mslist = [f.strip() for f in input_mslist]
     inparmdbs = [os.path.join(ms, parmdb_name) for ms in input_mslist]
 
+    # Sort the parmdbs by start time (needed for the timewidth checks below)
+    if len(inparmdbs) > 1:
+        start_times = []
+        for i, inparmdb in enumerate(inparmdbs):
+            pdb_in = pdb.parmdb(inparmdb)
+            parmname = pdb_add.getNames()[0]
+            parms = pdb_in.getValuesGrid(parmname)
+            start_times.append(parms[parmname]['times'][0])
+            pdb_in = False
+    inparmdbs = np.array(inparmdbs)[np.argsort(start_times)].tolist()
+    start_times.sort()
+
     if type(clobber) is str:
         if clobber.lower() == 'true':
             clobber = True
@@ -45,46 +57,31 @@ def main(input_mslist, parmdb_name, outparmdb, clobber=True):
             os.system('rm -rf {0}'.format(outparmdb))
         else:
             return
-    os.system('cp -r {0} {1}'.format(inparmdbs[0], outparmdb))
+    pdb_concat = pdb.parmdb(outparmdb, create=True)
 
-### old copy, which puts all values into the same domain (i.e. ignoring the timestamps)
-#    if len(inparmdbs) > 1:
-#        pdb_concat = lofar.parmdb.parmdb(outparmdb)
-#        for inparmdb in inparmdbs[1:]:
-#            pdb = lofar.parmdb.parmdb(inparmdb)
-#            for parmname in pdb.getNames():
-#                v = pdb.getValuesGrid(parmname)
-#                pdb_concat.addValues(v.copy())
-#        pdb_concat.flush()
+    for i, inparmdb in enumerate(inparmdbs):
+        pdb_add = pdb.parmdb(inparmdb)
 
-### version with TAQL, which avoids the previous problem, but may introduce new ones
-#    if len(inparmdbs) > 1:
-#        for inparmdb in inparmdbs[1:]:
-#            pt.taql('insert into '+outparmdb+' select from '+inparmdb)
-#    outdb = pt.table(outparmdb,readonly=False,ack=False)
-#    errdesc = outdb.getcoldesc('ERRORS')
-#    errdesc['name'] = 'ERRORS'
-#    outdb.removecols('ERRORS')
-#    outdb.addcols(errdesc)
-#    outdb.close()
+        for parmname in pdb_add.getNames():
+            parms = pdb_add.getValuesGrid(parmname)
 
-### version which adds new data by generating a new data-holder first
-    if len(inparmdbs) > 1:
-        pdb_concat = pdb.parmdb(outparmdb)
-        for inparmdb in inparmdbs[1:]:
-            pdb_add = pdb.parmdb(inparmdb)
-            for parmname in pdb_add.getNames():
-                parms = pdb_add.getValuesGrid(parmname)
-                ValueHolder = pdb_concat.makeValue(values=parms[parmname]['values'],
-                                                   sfreq=parms[parmname]['freqs'],
-                                                   efreq=parms[parmname]['freqwidths'],
-                                                   stime=parms[parmname]['times'],
-                                                   etime=parms[parmname]['timewidths'],
-                                                   asStartEnd=False)
-                pdb_concat.addValues(parmname,ValueHolder)       
-            pdb_concat.flush()
-            pdb_add = False 
-        pdb_concat = False
+            # Adjust last timewidth if necessary, as DPPP GainCal (as of 2.16.4) does not
+            # truncate last solution timewidth to end of MS
+            if i < len(inparmdbs[1:]) - 1:
+                inter_chunk_timewidth = start_times[i+1] - parms[parmname]['times'][-1]
+                if inter_chunk_timewidth < parms[parmname]['timewidths'][-1]:
+                    parms[parmname]['timewidths'][-1] = inter_chunk_timewidth
+
+            ValueHolder = pdb_concat.makeValue(values=parms[parmname]['values'],
+                                               sfreq=parms[parmname]['freqs'],
+                                               efreq=parms[parmname]['freqwidths'],
+                                               stime=parms[parmname]['times'],
+                                               etime=parms[parmname]['timewidths'],
+                                               asStartEnd=False)
+            pdb_concat.addValues(parmname, ValueHolder)
+        pdb_concat.flush()
+        pdb_add = False
+    pdb_concat = False
 
 
 if __name__ == '__main__':
