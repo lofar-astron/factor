0fdee71280099e3ff73054f103a207a5a3ae2a1f
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-06-22 16:27:53 +0200

    Add model data to clean-up list

--- factor/operations/facet_ops.py
+++ factor/operations/facet_ops.py
@@ -417,7 +417,8 @@ class FacetImage(Operation):
         # Delete temp data
         self.direction.cleanup_mapfiles = [
             os.path.join(self.pipeline_mapfile_dir, 'concat_averaged_input.mapfile'),
-            os.path.join(self.pipeline_mapfile_dir, 'image1.mapfile')]
+            os.path.join(self.pipeline_mapfile_dir, 'image1.mapfile'),
+            os.path.join(self.pipeline_mapfile_dir, 'corrupt_final_model.mapfile')]
         if not self.parset['keep_avg_facet_data'] and self.direction.name != 'target':
             # Add averaged calibrated data for the facet to files to be deleted.
             # These are only needed if the user wants to reimage by hand (e.g.,
