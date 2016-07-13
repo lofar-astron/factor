0c763ea4d8eb354fd09288461b8a9282fdaca5f7
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-07-13 21:40:28 +0200

    Remove parmdb adjustment

--- factor/scripts/merge_parmdbs_in_time.py
+++ factor/scripts/merge_parmdbs_in_time.py
@@ -68,22 +68,6 @@ def main(input_mslist, parmdb_name, outparmdb, clobber=True):
         pdb_add = pdb.parmdb(inparmdb)
         parms = pdb_add.getValuesGrid('*')
         for parmname in pdb_add.getNames():
-            # Adjust last timewidth if necessary, as DPPP GainCal (as of 2.16.4) does not
-            # truncate last solution timewidth to end of MS
-            if i < len(inparmdbs) - 1:
-                inter_chunk_timewidth = start_times_dicts[i+1][parmname] - parms[parmname]['times'][-1]
-                if inter_chunk_timewidth < parms[parmname]['timewidths'][-1]:
-                    parms[parmname]['timewidths'][-1] = inter_chunk_timewidth
-
-                    # Adjust following chunk as well
-                    pdb_next = pdb.parmdb(inparmdbs[i+1])
-                    parms_next = pdb_next.getValuesGrid(parmname)
-                    parms_next[parmname]['timewidths'][0] = inter_chunk_timewidth
-                    pdb_next.deleteValues(parmname)
-                    pdb_next.addValues(parmname, parms_next[parmname])
-                    pdb_next.flush()
-                    pdb_next = False
-
             ValueHolder = pdb_concat.makeValue(values=parms[parmname]['values'],
                                                sfreq=parms[parmname]['freqs'],
                                                efreq=parms[parmname]['freqwidths'],
