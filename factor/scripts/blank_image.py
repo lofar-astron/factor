8db190642c772d5efe5c6b652c877dea74264412
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-11-15 09:54:10 +0100

    Add missing arg

--- factor/scripts/blank_image.py
+++ factor/scripts/blank_image.py
@@ -117,7 +117,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=descriptiontext, formatter_class=RawTextHelpFormatter)
     parser.add_argument('input_image_file', help='Filename of input image')
     parser.add_argument('vertices_file', help='Filename of vertices file')
-    parser.add_argument('image_is_wsclean_model', help='True if input is WSClean model root')
+    parser.add_argument('output_image_file', help='Filename of output image')
+    parser.add_argument('image_is_wsclean_model', help='True if input is WSClean model root', type=bool, default=False)
     parser.add_argument('-b', '--blank_value', help='value for blank pixesl', type=str, default='zeros')
     args = parser.parse_args()
     main(args.input_image_file, args.vertices_file, args.output_image_file,
