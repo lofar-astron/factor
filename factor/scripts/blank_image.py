bb81da804069a56445ee0bda5b9d86fcb8fa86ea
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-11-15 11:17:28 +0100

    Tweak help text

--- factor/scripts/blank_image.py
+++ factor/scripts/blank_image.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
     parser.add_argument('vertices_file', help='Filename of vertices file')
     parser.add_argument('output_image_file', help='Filename of output image')
     parser.add_argument('image_is_wsclean_model', help='True if input is WSClean model root', type=bool, default=False)
-    parser.add_argument('-b', '--blank_value', help='value for blank pixesl', type=str, default='zeros')
+    parser.add_argument('-b', '--blank_value', help='value for blank pixesl', type=str, default='zero')
     args = parser.parse_args()
     main(args.input_image_file, args.vertices_file, args.output_image_file,
          blank_value=args.blank_value, image_is_wsclean_model=args.image_is_wsclean_model)
