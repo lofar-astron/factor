3a6e550b39648282867960f11f57fd1f74e5f5d8
David Rafferty <drafferty@hs.uni-hamburg.de>
2016-11-18 11:44:09 +0100

    Add back check for low amplitudes

--- factor/scripts/smooth_amps_spline.py
+++ factor/scripts/smooth_amps_spline.py
@@ -518,6 +518,11 @@ def main(instrument_name, instrument_name_smoothed, normalize=True, plotting=Fal
                     phase = numpy.arctan2(imag, real)
                     amp  = numpy.copy(numpy.sqrt(real**2 + imag**2))
 
+                    # Clip extremely low amplitude solutions to prevent very high
+                    # amplitudes in the corrected data
+                    low_ind = numpy.where(amp < 0.2)
+                    amp[low_ind] = 0.2
+
                     parms[gain + ':' + pol + ':Real:'+ antenna]['values'][:, chan] = numpy.copy(amp *
                         numpy.cos(phase) * norm_factor)
                     parms[gain + ':' + pol + ':Imag:'+ antenna]['values'][:, chan] = numpy.copy(amp *
