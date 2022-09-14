7f15c461c8ac7c9569c95ea78ea4e9f1cd7cb7a1
Marcel Loose <loose@astron.nl>
2022-09-14 20:54:25 +0200

    Complete rewrite of Dockerfile

--- Docker/build.sh
+++ Docker/build.sh
@@ -3,12 +3,13 @@ set -e
 
 COMMUNITY="astronrd"
 REPOSITORY="factor"
+BRANCH=$(git branch --show-current)
 TAG=$(git describe --tags)
 
-cd $(dirname ${0})
+cd "$(dirname "${0}")"
 
-docker build -t ${COMMUNITY}/${REPOSITORY}:${TAG} .
-docker tag ${COMMUNITY}/${REPOSITORY}:${TAG} ${COMMUNITY}/${REPOSITORY}:latest
+docker build -t "${COMMUNITY}/${REPOSITORY}:${TAG}" .
+docker tag "${COMMUNITY}/${REPOSITORY}:${TAG}" "${COMMUNITY}/${REPOSITORY}:${BRANCH}"
 
-docker push ${COMMUNITY}/${REPOSITORY}:${TAG}
-docker push ${COMMUNITY}/${REPOSITORY}:latest
+docker push "${COMMUNITY}/${REPOSITORY}:${TAG}"
+docker push "${COMMUNITY}/${REPOSITORY}:${BRANCH}"
